986abab9cc946b391a887e74dd5287130d5e9660
Manuel Silva Nieves <msnieves09@gmail.com>
2025-04-15 12:25:03 -0400

    Decoder & Menu
    Allows the user to decode the encoded phrase and adds a menu

--- src/package1/Encoder.java
+++ src/package1/Encoder.java
@@ -4,12 +4,26 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Encoder {
-	LocalTime currentTime = LocalTime.now();
-	int timeSum = currentTime.getHour() + currentTime.getMinute() + currentTime.getSecond();
+	static LocalTime currentTime = LocalTime.now();
+	static int timeSum = 0;
 	Character[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	//To do: Turn array into circular array
+	public static LocalTime time() {
+		return currentTime;
+		
+	} 
+	public int check(int n) {
+		if(n<0) {
+		return check(n+26);
+		}
+		else {
+			return n;
+		}
+	}
 	public String Encode(String ah) {
 		String outut= "";
 		String uppercase_Input = ah.toUpperCase();
+		timeSum = currentTime.getHour() + currentTime.getMinute() + currentTime.getSecond();
 		for (int i = 0;i < uppercase_Input.length(); i++){
 			for(int j = 0;j< alphabet.length; j++) {
 		    if(uppercase_Input.charAt(i) == alphabet[j]) {
@@ -17,19 +31,51 @@ public class Encoder {
 		    	}
 		    	}
 			}
-		return outut;
+		return outut + " " + time();
 	}
-	public int Decode() {
-		return 0;
+	public String Decode(String oh) {
+		String stuff[] = oh.split("[\\s\\:\\.]+");
+		String pew = "";
+		int decodeTimeSum = Integer.parseInt(stuff[1]) + Integer.parseInt(stuff[2]) + Integer.parseInt(stuff[3]);
+		for (int i = 0;i < oh.length(); i++){
+			for(int j = 0;j< alphabet.length; j++) {
+				if(oh.charAt(i) == alphabet[j]) {
+					if(j-decodeTimeSum<0){
+						pew+=(alphabet[check(j-decodeTimeSum)]);
+						}
+					else {
+					pew+=(alphabet[(j - decodeTimeSum)]);
+					}
+				}
+			}								
+		}
+		return pew;
 	}
 public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
-	String input = scanner.nextLine();
-	
 	Encoder Encodel = new Encoder();
+	String stringInput;
+	System.out.println("User options are:\n");
+	System.out.println("[0] Encryption");
+	System.out.println("[1] Decryption");
+	System.out.println();
 	
-	System.out.print(Encodel.Encode(input));
+	int input = scanner.nextInt();
+	
+	if(input == 0) {
+	System.out.println("What would you like to encrypt");
+	stringInput = scanner.nextLine();
+	stringInput = scanner.nextLine();
+	System.out.print(Encodel.Encode(stringInput));
 	scanner.close();
+	}
+	else{
+	System.out.println("What would you like to decrypt");
+	stringInput = scanner.nextLine();
+	stringInput = scanner.nextLine();
+	System.out.print(Encodel.Decode(stringInput));
+	scanner.close();
+	}
 }
 }
 
